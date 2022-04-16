5f577cadd777300920c2f6abf95212d0999620c3
Remigiusz Kocjan <r.kocjan@softwarethings.pro>
2022-04-16 14:06:51 +0200

    init

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <main class="container">
+    <h1>TESTING</h1>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+};
+</script>
+
+<style>
+body {
+  text-align: center;
+}
+</style>
